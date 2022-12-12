0ab59b5b98d868f8f1ce4734adeb31c7afcb14ec
owl410 <410owl@gmail.com>
2022-12-12 07:34:13 +0000

    Загрузить файлы 'bspwm/my_bspwm/.config/cmus'

---
+++ bspwm/my_bspwm/.config/cmus/lib.pl
@@ -0,0 +1,146 @@
+/home/owl/home/Music/AK - Discovery.mp3
+/home/owl/home/Music/Adelitas Way - Drifting.mp3
+/home/owl/home/Music/Alex Midi - Big In Japan.mp3
+/home/owl/home/Music/Aloe Blacc - I Need A Dollar (Live In Studio).mp3
+/home/owl/home/Music/Anastacia - Left Outside Alone.mp3
+/home/owl/home/Music/Bugseed - Pizza Pizza.mp3
+/home/owl/home/Music/DXXDLY - Be Afraid.mp3
+/home/owl/home/Music/Da Buzz - Wonder Where You Are.mp3
+/home/owl/home/Music/Enjoykin - Не унывайте пацаны.mp3
+/home/owl/home/Music/Icon For Hire - Make A Move.mp3
+/home/owl/home/Music/JAY-Z feat. Alicia Keys - Empire State Of Mind.mp3
+/home/owl/home/Music/Kamandi - Deans.mp3
+/home/owl/home/Music/Kyau & Albert - Memory Lane.mp3
+/home/owl/home/Music/LVL1 - FVN.mp3
+/home/owl/home/Music/Lacuna Coil - Aeon.mp3
+/home/owl/home/Music/Lacuna Coil - Angel's Punishment.mp3
+/home/owl/home/Music/Lacuna Coil - Daylight Dancer.mp3
+/home/owl/home/Music/Lacuna Coil - Entwined.mp3
+/home/owl/home/Music/Lacuna Coil - Self Deception.mp3
+/home/owl/home/Music/Lacuna Coil - Swamped.mp3
+/home/owl/home/Music/Lacuna Coil - Tight Rope.mp3
+/home/owl/home/Music/Metallica - Until It Sleeps.mp3
+/home/owl/home/Music/Michael Cassette - Pangaea (Envotion Remix).mp3
+/home/owl/home/Music/Mylène Farmer, Moby - Crier La Vie (Slipping Away).mp3
+/home/owl/home/Music/NBSPLV - Lost Soul.mp3
+/home/owl/home/Music/Norrin Radd - Never To Return.mp3
+/home/owl/home/Music/Opening - Enen no Shouboutai.mp3
+/home/owl/home/Music/PRXSXNT - Lights .mp3
+/home/owl/home/Music/Paul van Dyk - Forbidden Fruit.mp3
+/home/owl/home/Music/Peterpan - Menghapus Jejakmu.mp3
+/home/owl/home/Music/Radical Face - Welcome Home, Son.mp3
+/home/owl/home/Music/Ray Parker, Jr. - Ghostbusters.mp3
+/home/owl/home/Music/Sabrina Salerno - Erase Rewind.mp3
+/home/owl/home/Music/Shounanno Kaze - Grand Blue.mp3
+/home/owl/home/Music/Sidewalks and Skeletons - Goth.mp3
+/home/owl/home/Music/Soul Square - That Swing.mp3
+/home/owl/home/Music/Spice Girls - Viva Forever.mp3
+/home/owl/home/Music/Suzanne Vega - Tom's Diner.mp3
+/home/owl/home/Music/The Beatles Meet Queen - Bohemian Rhapsody.mp3
+/home/owl/home/Music/The Gypsy Queens feat. Madeleine Peyroux - L'Americano.mp3
+/home/owl/home/Music/The Weeknd - Save Your Tears.mp3
+/home/owl/home/Music/Thrice - Death from Above.mp3
+/home/owl/home/Music/Umbrella - Vl4s.mp3
+/home/owl/home/Music/Vacant - Seeker.mp3
+/home/owl/home/Music/Vestron Vulture - Homecoming.mp3
+/home/owl/home/Music/XANVALA - Dearest.mp3
+/home/owl/home/Music/XL Middleton - Either It's All Right Now (Or It Soon Will Be).mp3
+/home/owl/home/Music/atmosfer/4th Front - Calista.mp3
+/home/owl/home/Music/atmosfer/4th Front - Destination Stars.mp3
+/home/owl/home/Music/atmosfer/4th Front - Enphonic.mp3
+/home/owl/home/Music/atmosfer/AK - Another Place.mp3
+/home/owl/home/Music/atmosfer/Andy Leech - Rene.mp3
+/home/owl/home/Music/atmosfer/Astral - Runaway.mp3
+/home/owl/home/Music/atmosfer/Atlantic Cascade - Sea.mp3
+/home/owl/home/Music/atmosfer/Aurora B.Polaris - As Tears We Fall.mp3
+/home/owl/home/Music/atmosfer/Aurora Night - Lost.mp3
+/home/owl/home/Music/atmosfer/Bucky - Secret.mp3
+/home/owl/home/Music/atmosfer/CMA - You're Not Alone.mp3
+/home/owl/home/Music/atmosfer/Ennja - Kyoto Nights.mp3
+/home/owl/home/Music/atmosfer/Fluidified - Riversilvers - A Moment Of Silence.mp3
+/home/owl/home/Music/atmosfer/Happy Machines - Falling.mp3
+/home/owl/home/Music/atmosfer/Insight Music - Phelian - Dreamlore.mp3
+/home/owl/home/Music/atmosfer/Kaj. - Journey.mp3
+/home/owl/home/Music/atmosfer/Katie Melua - The Flood (Jakwob Remix).mp3
+/home/owl/home/Music/atmosfer/Kazukii - Be There For Me.mp3
+/home/owl/home/Music/atmosfer/Lady Gaga - Paparazzi (DJ Zar remix).mp3
+/home/owl/home/Music/atmosfer/MARION - High Hopes.mp3
+/home/owl/home/Music/atmosfer/Michael FK - In Our Hands.mp3
+/home/owl/home/Music/atmosfer/Norbert Woodson - I was too weak to love you.mp3
+/home/owl/home/Music/atmosfer/ODESZA feat. Sasha Sloan - Falls (Golden Features Remix).mp3
+/home/owl/home/Music/atmosfer/Onykia - Inverted Past.mp3
+/home/owl/home/Music/atmosfer/Oscuro - Give And Take.mp3
+/home/owl/home/Music/atmosfer/Oscuro - Time Stood Still.mp3
+/home/owl/home/Music/atmosfer/Pensees - Dreaveler.mp3
+/home/owl/home/Music/atmosfer/Pensees - Murky.mp3
+/home/owl/home/Music/atmosfer/Phelian + Xandra - Daphne.mp3
+/home/owl/home/Music/atmosfer/SIB3RIAN PLAGU3 х Αποстол Тимοфей - I continue.mp3
+/home/owl/home/Music/atmosfer/SOHO Palace - Da Vosk Docta - Białe Złoto.mp3
+/home/owl/home/Music/atmosfer/Sensi Affect - Ya Ya.mp3
+/home/owl/home/Music/atmosfer/Shah - Implacable.mp3
+/home/owl/home/Music/atmosfer/Sibewest - Control.mp3
+/home/owl/home/Music/atmosfer/Sidewalks and Skeletons - Scope.mp3
+/home/owl/home/Music/atmosfer/Summer of Haze - Meditative Lk.mp3
+/home/owl/home/Music/atmosfer/T A T U - show me love lemniscata remix.mp3
+/home/owl/home/Music/atmosfer/Trastler, Undine - Letter of Love.mp3
+/home/owl/home/Music/atmosfer/Valdhyr - Dawn.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Ciara.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Escape.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - High Rise.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - One More Chance.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Transit.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Trauma.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Wanderer.mp3
+/home/owl/home/Music/atmosfer/Whitewildbear - Come Back.mp3
+/home/owl/home/Music/atmosfer/Whitewildbear - Iwaly.mp3
+/home/owl/home/Music/atmosfer/Whitewildbear - One Day.mp3
+/home/owl/home/Music/atmosfer/Yellow Claw feat. Rochelle - Shotgun.mp3
+/home/owl/home/Music/atmosfer/barnacle boi - coming home.mp3
+/home/owl/home/Music/atmosfer/gin$eng - only you.mp3
+/home/owl/home/Music/atmosfer/skeler - tokyo.mp3
+/home/owl/home/Music/atmosfer/t.A.T.u. - Нас Не Догонят (#Witch House).mp3
+/home/owl/home/Music/atmosfer/† ŦĦ€ ǤΔΜ€Ş Ŵ€ ƤŁΔ¥ † - Riversilvers - Sun In Your Eyes.mp3
+/home/owl/home/Music/atmosfer/∆XIUS LIИК - Нас Нǝ Дøгøня†.mp3
+/home/owl/home/Music/atmosfer/ＢＡＤ ＴＲ!Ｐ - t.A.T.u. - Нас не догонят (Witch House Remix).mp3
+/home/owl/home/Music/bbno$, Diamond Pistols, BENEE - help herself (with BENEE).mp3
+/home/owl/home/Music/c152 - Tell Me Now.mp3
+/home/owl/home/Music/dj-Jo - Kimi No Sei.mp3
+/home/owl/home/Music/polnalyubvi - Где ты.mp3
+/home/owl/home/Music/psyagesh - Beautiful Night.mp3
+/home/owl/home/Music/pulse - tell me x jhené aiko.mp3
+/home/owl/home/Music/Беспокойник - Снова.mp3
+/home/owl/home/Music/atmosfer/CMA - Escape It All.mp3
+/home/owl/home/Music/atmosfer/Krewella - Lights & Thunder (Kyle Braun TRAP Bootleg).mp3
+/home/owl/home/Music/atmosfer/Spaarkey - Success.mp3
+/home/owl/home/Music/atmosfer/Tony Igy - It`s Lovely (Spaarkey remix).mp3
+/home/owl/home/Music/atmosfer/CMA - So Far Away (Instrumental).mp3
+/home/owl/home/Music/atmosfer/Aes Dana feat. Miktek - Evenfall.mp3
+/home/owl/home/Music/atmosfer/Aiko - Crystal.mp3
+/home/owl/home/Music/atmosfer/AK & Sublab - Saudade.mp3
+/home/owl/home/Music/atmosfer/BEΛR_FVCE - Bruh Bruh.mp3
+/home/owl/home/Music/atmosfer/Mimi Page ft. Clark Kent - Life.mp3
+/home/owl/home/Music/atmosfer/myzuka.ru_11_i_can_t_stop.mp3
+/home/owl/home/Music/atmosfer/Fort Minor - Where'd You Go (AudioBlood Remix).mp3
+/home/owl/home/Music/atmosfer/Georgy Om - Nas ne dogonyat.mp3
+/home/owl/home/Music/atmosfer/HENTAI BOYS - LIGHT.mp3
+/home/owl/home/Music/atmosfer/Kaj. - Flashback.mp3
+/home/owl/home/Music/atmosfer/Kopud - Broken Piano (Original Mix) (DUBSTEP).mp3
+/home/owl/home/Music/atmosfer/Lemnistaca - Show me love.mp3
+/home/owl/home/Music/atmosfer/Linkin Park - Numb (Dubstep Cover).mp3
+/home/owl/home/Music/atmosfer/Modern Tolking - Cheri, Cheri Lady (PRITON BEATZ Remix).mp3
+/home/owl/home/Music/atmosfer/MYSTXRIVL - WastingMyLife.mp3
+/home/owl/home/Music/atmosfer/Nirvana, Mujuice, Tori Amos - Smells like teen pussy.mp3
+/home/owl/home/Music/atmosfer/P.Diddy feat Dirty Money - Coming Home (Spaarkey Remix).mp3
+/home/owl/home/Music/atmosfer/Phelian - The Only Thing.mp3
+/home/owl/home/Music/atmosfer/Playmen ft. Demy - Fallin (Spaarkey remix).mp3
+/home/owl/home/Music/atmosfer/ペニス - She played me.mp3
+/home/owl/home/Music/atmosfer/Seven Lions feat. Fiora - Days to Come (Spaarkey remix).mp3
+/home/owl/home/Music/atmosfer/Atlantic Cascade - Echoes.mp3
+/home/owl/home/Music/atmosfer/Sqz Me - Once .mp3
+/home/owl/home/Music/atmosfer/Synkro - Acceptance.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - December.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Let Off.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Deep Down.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Left Behind.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Trainwreck.mp3
+/home/owl/home/Music/atmosfer/VΛCΛNT - Nocturnal.mp3
