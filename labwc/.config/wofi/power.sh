f5309326575cdcbf404939790369bd607376580e
owl410 <410owl@gmail.com>
2023-04-24 19:30:50 +0000

    Обновить 'labwc/.config/wofi/power.sh'

--- labwc/.config/wofi/power.sh
+++ labwc/.config/wofi/power.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+entries="⇠ Logout\n⏾ Suspend\n Reboot\n⏻ Shutdown"
 
 selected=$(echo -e $entries|wofi --width 100 --height 150 --xoffset 1764 --dmenu --cache-file /dev/null -c /home/owl/.config/wofi/power -s /home/owl/.config/wofi/power.css | awk '{print tolower($2)}')
 
